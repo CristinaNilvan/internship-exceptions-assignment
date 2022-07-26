e7bfb100403a8fedb8037327a7260e59cd239432
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-26 14:30:41 +0300

    Update

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project;
+
+Ingredient ingredient01 = new Ingredient(1, "faina", 60, 40);
+Ingredient ingredient02 = new Ingredient(2, "lapte", 55, 50);
+Ingredient ingredient03 = new Ingredient(3, "zahar", 45, 70);
+Ingredient ingredient04 = new Ingredient(4, "piersica", 30, 60);
+
+var ingredientList = new List<Ingredient>();
+ingredientList.Add(ingredient01);
+ingredientList.Add(ingredient02);
+ingredientList.Add(ingredient03);
+ingredientList.Add(ingredient04);
+
+var recipe = new Recipe(1, "Prajitura cu piersici", 190, ingredientList);
+
+var auxIngredient = recipe.GetAnIngredient(6);
+Console.WriteLine(auxIngredient.ToString());
--- Project/Project/Recipe.cs
+++ Project/Project/Recipe.cs
@@ -8,24 +8,26 @@ namespace Project
 {
     internal class Recipe
     {
-        public Recipe(int id, string? name, string? author, string? description, RecipeType? type, int calories,
-                List<Ingredient>? ingredients)
+        public Recipe(int id, string? name, int calories, List<Ingredient>? ingredients)
         {
             Id = id;
             Name = name;
-            Author = author;
-            Description = description;
-            Type = type;
             Calories = calories;
             Ingredients = ingredients;
         }
 
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Author { get; set; }
-        public string? Description { get; set; }
-        public RecipeType? Type { get; set; }
         public int Calories { get; set; }
         public List<Ingredient>? Ingredients { get; set; }
+
+        //Create methods which checks input arguments and throws exceptions
+        public Ingredient GetAnIngredient(int number)
+        {
+            if (number > Ingredients.Count)
+                throw new ArgumentOutOfRangeException();
+
+            return Ingredients.ElementAt(number);
+        }
     }
 }
