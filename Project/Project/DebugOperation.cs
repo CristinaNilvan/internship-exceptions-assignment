39c2f0f94030dc5c75c274f5d7fa41f6ff506c3f
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-26 16:18:59 +0300

    Add DebugOperation

---
+++ Project/Project/DebugOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class DebugOperation
+    {
+        public static void Action(bool foundException)
+        {
+            Debug.WriteLineIf(foundException, "An exception has occured!");
+        }
+    }
+}
