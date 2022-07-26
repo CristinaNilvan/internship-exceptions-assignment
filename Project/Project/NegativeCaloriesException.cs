8eb453d33bf2249a89facc9980e74925b5aa18d2
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-26 15:15:42 +0300

    Add custom exceptions

---
+++ Project/Project/NegativeCaloriesException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class NegativeCaloriesException : Exception
+    {
+        public NegativeCaloriesException()
+        {
+
+        }
+
+        public NegativeCaloriesException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Project/Project/NoMatchException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class NoMatchException : Exception
+    {
+        public NoMatchException()
+        {
+
+        }
+        public NoMatchException(string message) : base(message)
+        {
+
+        }
+    }
+}
